8d3787e9a2358d1872a4b96083cd0d96d1a50382
Mateusz <mateusz.harlos@gmail.com>
2020-10-20 15:16:18 +0100

    last commit

--- bus_lab_start_code/src/bus.py
+++ bus_lab_start_code/src/bus.py
@@ -2,9 +2,23 @@ class Bus:
     def __init__(self, route_number, destination):
         self.route_number = route_number
         self.destination = destination
-        # self.passengers = passengers[]
+        self.passengers = []
 
     def drive(self):
         return "Brum brum"
 
-    
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, person):
+        self.passengers.append(person)
+
+    def drop_off(self, person):
+        self.passengers.remove(person)
+
+    def empty(self):
+        self.passengers.clear()
+
+    def pick_up_from_stop(self, bus_stop):
+        for person in bus_stop.queue:
+            self.passengers.append(person)
