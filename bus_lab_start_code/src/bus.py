3430629cc42968f56b602ea906564e1d89342764
Mateusz <mateusz.harlos@gmail.com>
2020-10-20 14:26:19 +0100

    first commit

---
+++ bus_lab_start_code/src/bus.py
@@ -0,0 +1,10 @@
+class Bus:
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
+        self.destination = destination
+        # self.passengers = passengers[]
+
+    def drive(self):
+        return "Brum brum"
+
+    
